8bf39425372b2b58877903e935c0cfb3da41d1af
randler <randlersi@gmail.com>
2015-10-18 15:43:27 -0300

    Cadastro de Gasto

---
+++ CONAB/novoGasto/admin.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from CONAB.novoGasto.models import novoGasto
+
+class novoGastoAdmin(admin.ModelAdmin):
+    model = novoGasto
+    list_display = ['nome_posto','tipo_abastecido','preco_abastecido','litros_abastecido','data_abastecido','descricao','km_atual']
+    search_fields = ['nome_posto']
+    save_on_top = True
+admin.site.register(novoGasto, novoGastoAdmin)
--- CONAB/novoGasto/models.py
+++ CONAB/novoGasto/models.py
@@ -1,14 +1,12 @@
-from django.contrib import admin
+from django.db import models
 from django.db import models
 
-class novoGasto(models):
+class novoGasto(models.Model):
     gasto_id = models.AutoField(primary_key = True)
     litros_abastecido = models.DecimalField(max_digits=3, decimal_places= 3)
     tipo_abastecido = models.CharField(max_length=20)
     data_abastecido = models.DateField()
     preco_abastecido = models.DecimalField(max_digits=4, decimal_places=3)
-    nome_Posto = models.CharField(max_length=20)
-    km_atual = models.DecimalField(max_digits=6)
+    nome_posto = models.CharField(max_length=20)
+    km_atual = models.DecimalField(max_digits=6, decimal_places=6)
     descricao = models.CharField(max_length=60)
-
-
