23fa4290b493e286d56e1f323c01778b53e3a1e3
Juin <cremildolima@gmail.com>
2015-10-20 01:29:26 -0300

    Add opcoes padronizadas para tipo de combustivel.

--- CONAB/gasto/models.py
+++ CONAB/gasto/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 class Combustivel(models.Model):
+    TIPO_COMBUSTIVEL = (
+        (u'gasolina', u'Gasolina'),
+        (u'diesel',u'Diesel'),
+        (u'alcool',u'Alcool')
+        )
     gasto_id = models.AutoField(primary_key = True)
     litros_abastecido = models.FloatField()
-    tipo_abastecido = models.CharField(max_length=20)
+    tipo_abastecido = models.CharField(max_length=20,choices=TIPO_COMBUSTIVEL, verbose_name="Tipo de Combustivel")
     data_abastecido = models.DateField()
     preco_abastecido = models.FloatField()
     nome_posto = models.CharField(max_length=20)
@@ -14,4 +19,3 @@ class Conveniencia(models.Model):
     gasto_conv_id = models.AutoField(primary_key=True)
     descricao_conveniencia = models.CharField(max_length=50)
     preco_conveniencia = models.FloatField()
-
