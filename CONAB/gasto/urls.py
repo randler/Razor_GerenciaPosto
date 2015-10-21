6898a1e613468ce509846579ccd1362605c4aa69
randler <randlersi@gmail.com>
2015-10-20 22:18:57 -0300

    Login funcionando, mais ou menos

---
+++ CONAB/gasto/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+#from .views import index,index2
+
+urlpatterns = patterns('',
+
+#    url(r'^$' , index.as_view()),
+#    url(r'^index/$' , index2.as_view()),
+
+     url(r'^$', 'django.contrib.auth.views.login',
+         {'template_name': 'inicio/index.html'}, name='login'),
+
+    url(r'^fechar/$', 'django.contrib.auth.views.logout_then_login',
+         name='logout'),
+
+)
